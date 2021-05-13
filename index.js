5fd8cb8c4346b6160b0342f46818c3c13cf2f5b4
Luiz Henrique Carvalho Silveira <silveira.lh@gmail.com>
2021-05-13 17:20:06 -0300

    metodos POST de login e register

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const express =require('express');
 
-//models
-Users = require ('./model/User')
-
 app = express();
+
+//models
+User = require ('./model/User')
+
+
 
 app.set('view engine','hbs');
 app.set('views',path.join(__dirname,'view'));
@@ -16,26 +18,61 @@ app.use(express.urlencoded({
 	}));
 app.use(cookieParser());
 
-
+// rotas get
 app.get('/', (req, res) =>{
 	res.render('login');
-})
+});
 
 app.get('/newPicture', (req, res) =>{
 	res.render('newPicture',{username: req.cookies.login});
-})
+});
 
 app.get('/search', (req, res) =>{
 	res.render('search',{username: req.cookies.login});
-})
+});
 
 app.get('/register', (req, res) =>{
 	res.render('register');
-})
+});
 
+//rotas post
 app.post('/search', async (req,res) =>{
 	res.clearCookie('login');
 	res.redirect('/');
 })
+
+
+
+app.post('/',async (req,res) =>{
+	const 	username = req.body.username;
+	const password = req.body.password;
+	if(username!==""&&password!==""){
+		loginVerified = await User.logar(username,password);
+		if(loginVerified!="NULL"){
+			res.cookie('login');
+			if (loginVerified==1){
+				res.redirect('/newPicture');
+			}else{
+				res.redirect('/search');
+			}
+			return;
+		}else{
+			res.redirect('/');
+		}
+	}
+});
+
+app.post('/register',  async (req,res) =>{
+
+	const email = req.body.email;
+	const username = req.body.username;
+	const password = req.body.password;
+	console.log(username+password);
+	console.log(email + "  "+ username);
+	if(email!=="" && username!== "" && password!==""){
+		await User.cadastrar(email,username,password);
+	}
+	res.redirect('/search');
+});
 
 app.listen(3000);
--- model/User.js
+++ model/User.js
@@ -2,33 +2,42 @@ const Mongo = require('mongodb').MongoClient;
 
 module.exports = class Users{
 	
-    static async login(username,password){
-		const mongo = await Mongo.connect('mongodb://localhost/pod-api');
+    static async logar(username,password){
+		
+		const mongo = await Mongo.connect('mongodb://localhost/pod-api', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		});
 		const db = mongo.db();
-
-		let result =  await db.collection('users').find({
+		const answer =  await db.collection('users').find({
             username:username,
             password:password
         }).toArray();
-
 		mongo.close();
-		return result.length
+		if(answer.length>0){
+			return answer[0].admin;
+		}else{
+			return "NULL";
+		}
 	}
 
-	static async cadastrar(email, username,password){
-		const mongo = await Mongo.connect('mongodb://localhost/pod-api');
+	static async cadastrar(email, username, password){
+		const mongo = await Mongo.connect('mongodb://localhost/pod-api', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		});
 		const db = mongo.db();
-		let usernameArray =  await db.collection('users').find({username:username}).toArray();
-		let emailArray =  await db.collection('users').find({email:email}).toArray();
+		const usernameArray =  await db.collection('users').find({username:username}).toArray();
+		const emailArray =  await db.collection('users').find({email:email}).toArray();
 
 		if (usernameArray.length===0&&emailArray.length===0){
-			db.collection('users').insertOne({
+			await db.collection('users').insertOne({
 				email: email,
                 username:username,
                 password:password,
                 admin:0
             });
 		}
-		conn.close();
+		mongo.close();
 	}
 }
